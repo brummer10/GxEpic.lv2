6572a65b54d158ee19479ae0e27cdbd46f6adfc4
brummer10 <brummer-@web.de>
2021-03-22 19:07:58 +0100

    Fix issue #33 ON/OFF toggle button doesn't work on several plugins

--- gui/gx_epic_x11ui.c
+++ gui/gx_epic_x11ui.c
@@ -705,7 +705,6 @@ static void check_value_changed(gx_epicUI * const ui, int i, const float * const
 			ui->write_function(ui->controller,ui->controls[i].port,sizeof(float),0,value);
 		debug_print("send_controller_event for %i value %f\n",i,*(value));
 		send_controller_event(ui, i);
-		ui->block_event = -1;
 	}
 }
 
@@ -1076,6 +1075,7 @@ static void port_event(LV2UI_Handle handle, uint32_t port_index,
 		if (port_index == ui->controls[i].port) {
 			ui->block_event = (int)port_index;
 			check_value_changed(ui, i, &value);
+			ui->block_event = -1;
 		}
 	}
 }
