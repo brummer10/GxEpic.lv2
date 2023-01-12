b117d5b3fb8f7f5de93da6feae69719ad69d35f2
Thorsten Maerz <info@netztorte.de>
2023-01-12 19:29:17 +0100

    remove old gtk workaround (reset_request())

--- gui/gx_epic_x11ui.c
+++ gui/gx_epic_x11ui.c
@@ -659,12 +659,6 @@ static void controller_expose(const gx_epicUI * const ui,  const gx_controller *
 -----------------------------------------------------------------------
 ----------------------------------------------------------------------*/
 
-// hack to allow resizing below the initial size on gtk based hosts
-static void reset_request(gx_epicUI * const ui) {
-	ui->resize->ui_resize(ui->resize->handle, ui->width/1.4, ui->height/1.4);
-	ui->first_resize = false;
-}
-
 // resize the xwindow and the cairo xlib surface
 static void resize_event(gx_epicUI * const ui) {
 	XWindowAttributes attrs;
@@ -986,7 +980,6 @@ static void event_handler(gx_epicUI * const ui) {
 					break;
 					case Button2:
 						// click on the mouse wheel
-						if (ui->first_resize) reset_request(ui);
 						//debug_print("Button2 \n");
 					break;
 					case Button3:
